af4fa98b51a8339240a2fcf45aaf50c15c1cf47a
Jack Estelle <jackestelleunc@gmail.com>
2025-03-03 16:06:45 -0500

    Update identity.ts
    Tone, Owner Description, etc

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Porsche`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `German automobile manufacturer focused on delivering luxury performance vehicles since 1931`;
 
 export const AI_NAME: string = `Tom Targa`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Polite and Helpful`;
+export const AI_ROLE: string = `Customer Service Chatbot`;

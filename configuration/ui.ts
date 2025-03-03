bad1b368dbcadba05c4c58ac0b9a10feefa30ba2
Jack Estelle <jackestelleunc@gmail.com>
2025-03-03 16:22:06 -0500

    Update ui.ts HEADER

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Porsche AI`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Driving in its most beautiful form`;
 export const CLEAR_BUTTON_TEXT: string = `Back to the Starting Line`;

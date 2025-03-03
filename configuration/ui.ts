810a2d79964cbfcf07718c8f79fb4633c2217ffc
Jack Estelle <jackestelleunc@gmail.com>
2025-03-03 16:27:02 -0500

    Update ui.ts PAGE TITLE

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Porsche AI`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Driving in its most beautiful form`;
 export const CLEAR_BUTTON_TEXT: string = `Back to the Starting Line`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Porsche AI Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";

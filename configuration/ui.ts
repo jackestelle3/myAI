768b7d461a25646b040d5c5f4c3412641b32a6ac
Jack Estelle <jackestelleunc@gmail.com>
2025-03-17 15:16:40 -0400

    Update ui.ts to add a period

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Porsche AI Assistant`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Created by Jack Estelle for BUSI 488. No affiliation with Dr. Ing. h.c. F. Porsche AG`;
+export const FOOTER_MESSAGE: string = `Created by Jack Estelle for BUSI 488. No affiliation with Dr. Ing. h.c. F. Porsche AG.`;
 export const CLEAR_BUTTON_TEXT: string = `Back to the Starting Line`;
 export const PAGE_TITLE: string = `Porsche AI Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
